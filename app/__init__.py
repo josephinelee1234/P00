ad61e50ac7f2937443ad030c3c733ffbb0091d1d
AnnabelZhang1 <annabel.zhang1@gmail.com>
2021-10-30 12:37:14 -0400

    added run statement + updated devlo

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,15 @@
 #2021-10-27
 
 from flask import Flask
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route("/")       
+@app.route("/")
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
     return "No hablo queso!"
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()

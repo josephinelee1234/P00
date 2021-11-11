0d38596d63d19388bdd99600b5eb7b5aef242efc
hzhangg20 <hanzhang99999@gmail.com>
2021-11-11 13:42:36 -0500

    added try except blocks and fixed error where username was not shown in certain interaction

--- app/test.py
+++ app/test.py
@@ -161,24 +161,27 @@ def uploadNewStory():
     ''' Adds new story from createstory.html to database;
     Goes back to home.html '''
     if 'currentuser' in session:                #checks if user is in session
-        title = request.form['title']
-        content = request.form['content']
+        try:
+            title = request.form['title']
+            content = request.form['content']
 
-        if title == '':
-            return render_template('createstory.html', status="Please enter a story.", story=content)
+            if title == '':
+                return render_template('createstory.html', status="Please enter a story.", story=content)
 
-        if title in getValue('title','stories'): # checks if story title has already been used
-            return render_template('createstory.html', status="Duplicate story title- Please enter a different title.", story=content)
+            if title in getValue('title','stories'): # checks if story title has already been used
+                return render_template('createstory.html', status="Duplicate story title- Please enter a different title.", story=content)
 
-        else:
-            query = 'INSERT INTO ' + session['currentuser'] + ' VALUES(?)'
-            c.execute(query,[title])
+            else:
+                query = 'INSERT INTO ' + session['currentuser'] + ' VALUES(?)'
+                c.execute(query,[title])
 
-            query = 'INSERT INTO stories VALUES(?,?,?,?);'
-            c.execute(query,[title,content,content,session['currentuser']])
-            c.execute('SELECT * FROM stories;')
-            db.commit()
-            return render_template('home.html',user = session['currentuser'], status='Story successfully created', user_stories = get_user_stories(session['currentuser']))
+                query = 'INSERT INTO stories VALUES(?,?,?,?);'
+                c.execute(query,[title,content,content,session['currentuser']])
+                c.execute('SELECT * FROM stories;')
+                db.commit()
+                return render_template('home.html',user = session['currentuser'], status='Story successfully created', user_stories = get_user_stories(session['currentuser']))
+        except:
+            return render_template('home.html',user = session['currentuser'], status='ERROR', user_stories=get_user_stories(session['currentuser']))
 
     else:
         return render_template('login.html', status = 'Please log in to create a new story.')
@@ -189,10 +192,14 @@ def updateStory():
     ''' From home.html
     Sends user to updatestory.html '''
     if 'currentuser' in session:
-        query = 'SELECT title FROM stories'
-        c.execute(query)
-        rows = c.fetchall()
-        return render_template('updatestory.html', user = session['currentuser'],stories=rows)
+        try:
+            query = 'SELECT title FROM stories'
+            c.execute(query)
+            rows = c.fetchall()
+            return render_template('updatestory.html', user = session['currentuser'],stories=rows)
+        except:
+            return render_template('home.html',user = session['currentuser'], status='ERROR', user_stories=get_user_stories(session['currentuser']))
+
 
     return render_template('login.html', status = 'Please log in to update a story.')
 
@@ -202,24 +209,27 @@ def addToTitle():
     ''' From updatestory.html:
     Takes in title of a story and determines whether or not user can add to the story;
     Goes to addcontent.html '''
-    if 'title' in session:
-        session.pop('title')
-
     if 'currentuser' in session:
-        title = request.form['title']
-        titleList = getValue('title','stories')
-        if title in titleList:
-            if title in get_user_stories(session['currentuser']):
-                return render_template('home.html',user = session['currentuser'], status = 'You cannot add to stories you have already contributed to.', user_stories = get_user_stories(session['currentuser']))
+        try:
+            if 'title' in session:
+                session.pop('title')
+
+            title = request.form['title']
+            titleList = getValue('title','stories')
+            if title in titleList:
+                if title in get_user_stories(session['currentuser']):
+                    return render_template('home.html',user = session['currentuser'], status = 'You cannot add to stories you have already contributed to.', user_stories = get_user_stories(session['currentuser']))
+                else:
+                    query = 'SELECT latest FROM stories WHERE title = \'' + title + '\''
+                    c.execute(query)
+                    rows = c.fetchall()
+                    content = rows[0]
+                    session['title'] = title
+                    return render_template('addcontent.html',content = content, title = title)
             else:
-                query = 'SELECT latest FROM stories WHERE title = \'' + title + '\''
-                c.execute(query)
-                rows = c.fetchall()
-                content = rows[0]
-                session['title'] = title
-                return render_template('addcontent.html',content = content, title = title)
-        else:
-            return render_template('home.html', status = 'Story does not exist.', user_stories = getValue("title", session['currentuser']))
+                return render_template('home.html', user = session['currentuser'], status = 'Story does not exist.', user_stories = getValue("title"))
+        except:
+            return render_template('home.html',user = session['currentuser'], status = 'ERROR', user_stories = get_user_stories(session['currentuser']))
     else:
         return render_template('login.html', status = 'Please log in to update a story.')
 
@@ -230,32 +240,35 @@ def uploadUpdatedStory():
     Updates existing story in database with content from addcontent.html;
     Goes back to home.html '''
     if 'currentuser' in session:                #checks if user is in session
-        content = request.form['content']
-        title = session['title']
+        try:
+            content = request.form['content']
+            title = session['title']
 
-        query = 'SELECT latest FROM stories WHERE title = \'' + title + '\''
-        c.execute(query)
-        latest = c.fetchall()[0][0]
+            query = 'SELECT latest FROM stories WHERE title = \'' + title + '\''
+            c.execute(query)
+            latest = c.fetchall()[0][0]
 
-        query = 'SELECT content FROM stories WHERE title = \'' + title + '\''
-        c.execute(query)
-        current = c.fetchall()
-        updatedContent = current[0][0] + " " + content
+            query = 'SELECT content FROM stories WHERE title = \'' + title + '\''
+            c.execute(query)
+            current = c.fetchall()
+            updatedContent = current[0][0] + " " + content
 
-        query1 = "UPDATE stories SET content = \'" + updatedContent + "\' WHERE title = \'" + title + '\''
-        c.execute(query1)
-        db.commit()
+            query1 = "UPDATE stories SET content = \'" + updatedContent + "\' WHERE title = \'" + title + '\''
+            c.execute(query1)
+            db.commit()
 
 
-        query2 = "UPDATE stories SET latest = \'" + content + "\' WHERE title = \'" + title + '\''
-        c.execute(query2)
-        db.commit()
+            query2 = "UPDATE stories SET latest = \'" + content + "\' WHERE title = \'" + title + '\''
+            c.execute(query2)
+            db.commit()
 
-        #add to the list of stories that the user has worked on
-        query2 = 'INSERT INTO ' + session['currentuser'] + ' VALUES(?)'
-        c.execute(query2, [title])
-        db.commit()
-        return render_template('home.html',user = session['currentuser'], status='Story successfully updated', user_stories = get_user_stories(session['currentuser']))
+            #add to the list of stories that the user has worked on
+            query2 = 'INSERT INTO ' + session['currentuser'] + ' VALUES(?)'
+            c.execute(query2, [title])
+            db.commit()
+            return render_template('home.html',user = session['currentuser'], status='Story successfully updated', user_stories = get_user_stories(session['currentuser']))
+        except:
+            return render_template('home.html',user = session['currentuser'], status='ERROR', user_stories = get_user_stories(session['currentuser']))
 
     return render_template('login.html', status = 'Please log in to create a new story.')
 
@@ -265,17 +278,25 @@ def viewStory():
     ''' From home.html:
     Returns latest story content
     Goes to story.html '''
-    userTitles = get_user_stories(session['currentuser'])
-    title = request.form['title']
+    if 'currentuser' in session:                #checks if user is in session
+        try:
+            userTitles = get_user_stories(session['currentuser'])
+            title = request.form['title']
 
-    if title in userTitles:     #checks if user has contributed to requested story.
-        query = 'SELECT content FROM stories WHERE title = \'' + title + '\''
-        c.execute(query)
-        rows = c.fetchall()[0][0]
+            if title in userTitles:     #checks if user has contributed to requested story.
+                query = 'SELECT content FROM stories WHERE title = \'' + title + '\''
+                c.execute(query)
+                rows = c.fetchall()[0][0]
 
-        return render_template('story.html',title = title, content = rows)
+                return render_template('story.html',title = title, content = rows)
+            else:
+                return render_template('home.html',user = session['currentuser'], status='You may only view stories you have contributed to.', user_stories=get_user_stories(session['currentuser']))
+        except:
+            return render_template('home.html',user = session['currentuser'], status='ERROR', user_stories=get_user_stories(session['currentuser']))
+
     else:
-        return render_template('home.html',user = session['currentuser'], status='You may only view stories you have contributed to.', user_stories=get_user_stories(session['currentuser']))
+        return render_template('login.html', status = 'Please log in to view a new story.')
+
 
 
 
